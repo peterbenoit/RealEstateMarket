2baf8ec59ce82a130bb7c64871c400aadc744ab7
Pete Benoit <peterbenoit@gmail.com>
2024-08-13 14:40:52 -0400

    new endpoints

--- fetch-api-data.js
+++ fetch-api-data.js
@@ -9,7 +9,10 @@ const SERIES_IDS = [
 const API_URLS = [
     { name: 'ACTLISCOUFL', url: 'https://api.stlouisfed.org/fred/series/observations?series_id=ACTLISCOUFL&api_key=4aef95261705b5d131c5dba252bbdb45&file_type=json' },
     { name: 'MEDDAYONMARFL', url: 'https://api.stlouisfed.org/fred/series/observations?series_id=MEDDAYONMARFL&api_key=4aef95261705b5d131c5dba252bbdb45&file_type=json' },
-    { name: 'FLSTHPI', url: 'https://api.stlouisfed.org/fred/series/observations?series_id=FLSTHPI&api_key=4aef95261705b5d131c5dba252bbdb45&file_type=json' },
+    { name: 'ACTLISCOUMMFL', url: 'https://api.stlouisfed.org/fred/series/observations?series_id=FLSTHPI&api_key=4aef95261705b5d131c5dba252bbdb45&file_type=json' },
+    { name: 'ACTLISCOUGA', url: 'https://api.stlouisfed.org/fred/series/observations?series_id=FLSTHPI&api_key=4aef95261705b5d131c5dba252bbdb45&file_type=json' },
+    { name: 'MEDDAYONMARGA', url: 'https://api.stlouisfed.org/fred/series/observations?series_id=FLSTHPI&api_key=4aef95261705b5d131c5dba252bbdb45&file_type=json' },
+    { name: 'ACTLISCOUMMGA', url: 'https://api.stlouisfed.org/fred/series/observations?series_id=FLSTHPI&api_key=4aef95261705b5d131c5dba252bbdb45&file_type=json' }
     // Add other API endpoints as needed
 ];
 
